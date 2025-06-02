193d6550c432c59bf41acee0e41764646afb6fe4
manimehalaianandaraj <manimehalaiannakodi@gmail.com>
2025-06-02 12:20:25 +0530

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,21 @@
-import React, { useEffect, useState } from "react";
-import '../styles/App.css';
+import React from "react";
 
-const App = () => {
-
-  let [count, setCount] = useState(0)
+function App() {
+  const handleClick = () => alert('Button clicked!');
+  const handleChange = (e) => console.log(e.target.value);
 
   return (
-    <div class="ball">
-      <h1 class="count" ondoubleclick={() => { alert("cant edit it") }}>{count}</h1>
-      <button class='increment-button' onclick={() => { setCount(count + 1) }}>Increment</button>
+    <div className="app-container">
+      <label htmlFor="username">Username:</label>
+      <input
+        id="username"
+        type="text"
+        maxLength={10}
+        readOnly
+        onChange={handleChange}
+      />
+      <button onClick={handleClick}>Submit</button>
     </div>
-  )
+  );
 }
-
-
 export default App;
